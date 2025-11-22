ae3cbea3e172342eced434bfb87de281357af21c
José Maurício <mauricio.rodriguesdelima@gmail.com>
2025-11-22 19:43:39 -0300

    Estrutura de dados - Exercicio D + Desafio extra.

---
+++ src/estrutura_de_dados/exercicioD/ContagemParImpar.java
@@ -0,0 +1,50 @@
+package estrutura_de_dados.exercicioD;
+
+public class ContagemParImpar {
+
+    private int[] numeros;
+    private int par;
+    private int impar;
+
+    public ContagemParImpar() {
+    }
+
+    public ContagemParImpar(int[] numeros) {
+        this.numeros = numeros;
+    }
+
+    public void quantosParesImpares() {
+
+        par = 0;
+        impar = 0;
+
+        for (int numero : numeros) {
+            if (numero % 2 == 0) {
+                par++;
+            } else {
+                impar++;
+            }
+        }
+
+        System.out.println("Números pares: " + par +
+                "\nNumeros impares: " + impar);
+
+    }
+
+    public void porcentagemParImpar() {
+
+        double porcentagemPar = ((double) par / numeros.length) * 100;
+        double porcentagemImpar = ((double) impar / numeros.length) * 100;
+
+        System.out.printf("A porcentagem de números pares: %.2f %% %n" +
+                "A porcentagem de números impares: %.2f %%", porcentagemPar, porcentagemImpar);
+    }
+
+    public int[] getNumeros() {
+        return numeros;
+    }
+
+    public void setNumeros(int[] numeros) {
+        this.numeros = numeros;
+    }
+}
---
+++ src/estrutura_de_dados/exercicioD/ContagemParImparTest.java
@@ -0,0 +1,15 @@
+package estrutura_de_dados.exercicioD;
+
+public class ContagemParImparTest {
+
+    public static void main(String[] args) {
+
+        int[] arrayInteiros = {1, 2, 3, 4, 5, 6};
+
+        ContagemParImpar contagem =  new ContagemParImpar(arrayInteiros);
+
+        contagem.quantosParesImpares();
+        contagem.porcentagemParImpar();
+
+    }
+}
