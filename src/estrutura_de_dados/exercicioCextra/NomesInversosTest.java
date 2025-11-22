d6ed4a39ccb3a56908b85774229e1c7da78ebd86
José Maurício <mauricio.rodriguesdelima@gmail.com>
2025-11-22 18:50:28 -0300

    Estrutura de dados - Exercicio C - Desafio.

---
+++ src/estrutura_de_dados/exercicioCextra/NomesInversosTest.java
@@ -0,0 +1,15 @@
+package estrutura_de_dados.exercicioCextra;
+
+public class NomesInversosTest {
+
+    public static void main(String[] args) {
+
+        String[] nomes = {"Ana", "Bruno", "Carla", "Diego", "Eva"};
+
+        NomesInversos nomesInversos = new NomesInversos(nomes);
+
+        nomesInversos.exibirNomesInversos();
+
+        nomesInversos.adicionarNomes();
+    }
+}
